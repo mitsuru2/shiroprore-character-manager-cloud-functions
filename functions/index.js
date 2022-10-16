eeee2d92389680625b0d1644aa378cf3e4e9c2db
mitsuru2 <mitsuru.takahashi@live.jp>
2022-10-16 11:25:53 +0900

    Bug fix: internalTag was forgotten.

--- functions/index.js
+++ functions/index.js
@@ -246,3 +246,124 @@ exports.logCharacterTagsDeleteEvent = functions.region('asia-northeast1').firest
     data: deleteData,
   });
 });
+
+// ==============================================================================
+// Firestore: Characters
+//
+exports.logCharactersUpdateEvent = functions.region('asia-northeast1').firestore.document('/Characters/{documentId}').onUpdate((snap, context) => {
+  const newData = snap.after.data();
+  const prevData = snap.before.data();
+  let fieldName = '';
+  const fieldValues = [];
+
+  if (prevData.name !== newData.name) {
+    fieldName = 'name';
+    fieldValues.push(prevData.name);
+    fieldValues.push(newData.name);
+  } else if (prevData.type !== newData.type) {
+    fieldName = 'type';
+    fieldValues.push(prevData.type);
+    fieldValues.push(newData.type);
+  } else if (prevData.subType !== newData.subType) {
+    fieldName = 'subType';
+    fieldValues.push(prevData.subType);
+    fieldValues.push(newData.subType);
+  } else if (prevData.rarerity !== newData.rarerity) {
+    fieldName = 'rarerity';
+    fieldValues.push(prevData.rarerity);
+    fieldValues.push(newData.rarerity);
+  } else if (prevData.cost !== newData.cost) {
+    fieldName = 'cost';
+    fieldValues.push(prevData.cost);
+    fieldValues.push(newData.cost);
+  } else if (prevData.costKai !== newData.costKai) {
+    fieldName = 'costKai';
+    fieldValues.push(prevData.costKai);
+    fieldValues.push(newData.costKai);
+  } else if (prevData.weaponType !== newData.weaponType) {
+    fieldName = 'weaponType';
+    fieldValues.push(prevData.weaponType);
+    fieldValues.push(newData.weaponType);
+  } else if (isArrayChanged(prevData.geographTypes, newData.geographTypes)) {
+    fieldName = 'geographTypes';
+    fieldValues.push(prevData.geographTypes);
+    fieldValues.push(newData.geographTypes);
+  } else if (prevData.region !== newData.region) {
+    fieldName = 'region';
+    fieldValues.push(prevData.region);
+    fieldValues.push(newData.region);
+  } else if (isArrayChanged(prevData.voiceActors, newData.voiceActors)) {
+    fieldName = 'voiceActors';
+    fieldValues.push(prevData.voiceActors);
+    fieldValues.push(newData.voiceActors);
+  } else if (isArrayChanged(prevData.illustrators, newData.illustrators)) {
+    fieldName = 'illustrators';
+    fieldValues.push(prevData.illustrators);
+    fieldValues.push(newData.illustrators);
+  } else if (isArrayChanged(prevData.tags, newData.tags)) {
+    fieldName = 'tags';
+    fieldValues.push(prevData.tags);
+    fieldValues.push(newData.tags);
+  } else if (isArrayChanged(prevData.internalTags, newData.internalTags)) {
+    fieldName = 'internalTags';
+    fieldValues.push(prevData.internalTags);
+    fieldValues.push(newData.internalTags);
+  } else if (isArrayChanged(prevData.motifWeapons, newData.motifWeapons)) {
+    fieldName = 'motifWeapons';
+    fieldValues.push(prevData.motifWeapons);
+    fieldValues.push(newData.motifWeapons);
+  } else if (isArrayChanged(prevData.motifFacilities, newData.motifFacilities)) {
+    fieldName = 'motifFacilities';
+    fieldValues.push(prevData.motifFacilities);
+    fieldValues.push(newData.motifFacilities);
+  } else if (isArrayChanged(prevData.abilities, newData.abilities)) {
+    fieldName = 'abilities';
+    fieldValues.push(prevData.abilities);
+    fieldValues.push(newData.abilities);
+  } else if (isArrayChanged(prevData.abilitiesKai, newData.abilitiesKai)) {
+    fieldName = 'abilitiesKai';
+    fieldValues.push(prevData.abilitiesKai);
+    fieldValues.push(newData.abilitiesKai);
+  } else {
+    // Unexpected pattern. Log all data.
+    fieldName = 'all';
+    fieldValues.push(prevData);
+    fieldValues.push(newData);
+  }
+
+  functions.logger.log({
+    collection: 'Characters',
+    operation: 'Update',
+    docId: context.params.documentId,
+    updatedAt: newData.updatedAt.toDate().toISOString(),
+    updatedBy: newData.updatedBy,
+    field: fieldName,
+    prev: fieldValues[0],
+    new: fieldValues[1]});
+
+  return true;
+});
+
+exports.logCharactersCreateEvent = functions.region('asia-northeast1').firestore.document('/Characters/{documentId}').onCreate((snap, context) => {
+  const newData = snap.data();
+
+  functions.logger.log({
+    collection: 'Characters',
+    operation: 'Create',
+    docId: context.params.documentId,
+    updatedAt: newData.updatedAt.toDate().toISOString(),
+    updatedBy: newData.updatedBy,
+    data: newData,
+  });
+});
+
+exports.logCharactersDeleteEvent = functions.region('asia-northeast1').firestore.document('/Characters/{documentId}').onDelete((snap, context) => {
+  const deleteData = snap.data();
+
+  functions.logger.log({
+    collection: 'Characters',
+    operation: 'Delete',
+    docId: context.params.documentId,
+    data: deleteData,
+  });
+});
